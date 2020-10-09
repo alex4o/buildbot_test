a774a663d42eea68661e8ea65a33029454ef408e
Alexander Bonin <mrabonin@gmail.com>
2020-10-09 16:43:31 +0300

    Update .buildbot.sh

--- .buildbot.sh
+++ .buildbot.sh
@@ -1,10 +1,17 @@
 #/bin/sh
 
+set -e
+
+echo $USER
+echo $PWD
+
+~/${HOME}/cheri/output/sdk/bin/riscv64-unknown-freebsd13-clang --version
+
 echo Mine Crypto :D
 echo no sleep
 
 echo hey bors
-sleep 60
+# sleep 60
 echo Ready !
 
 # no docker installed :(
